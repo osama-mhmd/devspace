97b3b1d3c3c205687075bca5590fc8fbb3cedf5e
osama-mhmd <docosmo010@gmail.com>
2025-01-08 07:35:17 +0200

    fix: build

--- src/app/auth/error-field.tsx
+++ src/app/auth/error-field.tsx
@@ -1,7 +1,7 @@
 import { FieldError } from "react-hook-form";
+import * as m from "@/paraglide/messages";
 
 export default function Error({ error }: { error: FieldError | undefined }) {
-  // TODO: make this better
   if (!error) return;
   // @ts-ignore
   return <p className="error">{m[error.message]()}</p>;
