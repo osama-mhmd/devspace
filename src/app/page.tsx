6bab6f8c300af3740769f4ff5b62633eacdd6c13
Osama Mohammed <docosmo010@gmail.com>
2025-01-24 17:32:44 +0200

    chore: fourth design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,62 +4,55 @@ import { lilita } from "@/lib/fonts";
 import { validateRequest } from "@/db/auth";
 import { Circle, Star } from "lucide-react";
 import * as m from "@/paraglide/messages";
-import Rocket from "@/components/icons/rocket";
+import { Rocket } from "@/components/icons";
+import Image from "next/image";
 
 export default async function Home() {
   const { session } = await validateRequest();
 
   return (
-    <main
-      className="flex flex-col pt-28 h-screen bg-gradient-to-br from-gray-900 to-blue-900"
-      style={{
-        backgroundImage:
-          "radial-gradient(circle at center, #1e3a8a 10%, #111827 65%)",
-      }}
-    >
-      <div className="container flex items-center text-center flex-col gap-4">
-        <h1
-          className={
-            "text-5xl flex items-center gap-2 sm:text-7xl mb-0 " + lilita
-          }
-        >
-          DevSpace <Rocket />
-        </h1>
-        <p
-          className={
-            "text-muted-foreground text-2xl max-w-prose mb-2 " + lilita
-          }
-        >
-          {m.appDescription()}
-        </p>
-        <div className="flex flex-wrap items-center gap-2">
-          {!session && (
-            <>
-              <Button variant="outline" asChild>
-                <Link href="/auth/login">{m.login()}</Link>
-              </Button>
+    <main className="flex flex-col pt-28 h-screen bg-gradient-to-br from-gray-200 to-gray-300">
+      <div className="container flex justify-evenly gap-4">
+        <div className="flex flex-col max-w-md">
+          <h1
+            className={
+              "text-5xl flex items-center gap-2 sm:text-7xl mb-0 " + lilita
+            }
+          >
+            DevSpace <Rocket />
+          </h1>
+          <p className={"text-muted-foreground text-2xl my-4 " + lilita}>
+            {m.appDescription()}
+          </p>
+          <div className="flex flex-wrap items-center gap-2">
+            {!session && (
+              <>
+                <Button variant="outline" asChild>
+                  <Link href="/auth/login">See pricing</Link>
+                </Button>
+                <Button asChild>
+                  <Link href="/auth/register">Try it for free</Link>
+                </Button>
+              </>
+            )}
+            {session && (
               <Button asChild>
-                <Link href="/auth/register">{m.createAccount()}</Link>
+                <Link href="/app">{m.app()}</Link>
               </Button>
-            </>
-          )}
-          {session && (
-            <Button asChild>
-              <Link href="/app">{m.app()}</Link>
+            )}
+            <Button variant="link" className="underline flex gap-2">
+              <span className="relative">
+                <Star fill="white" className="p-0.5" />
+                <Circle
+                  fill="hsl(var(--primary))"
+                  className="-z-10 absolute top-0 left-0 w-full h-full"
+                />
+              </span>{" "}
+              {m.starOnGithub()}
             </Button>
-          )}
-          <Button variant="link" className="underline flex gap-2">
-            <span className="relative">
-              <Star fill="white" className="p-0.5" />
-              <Circle
-                fill="hsl(var(--primary))"
-                className="-z-10 absolute top-0 left-0 w-full h-full"
-              />
-            </span>{" "}
-            {m.starOnGithub()}
-          </Button>
+          </div>
         </div>
-        <div></div>
+        <Image width={500} height={500} alt="design" src="/project.ill.png" />
       </div>
     </main>
   );
