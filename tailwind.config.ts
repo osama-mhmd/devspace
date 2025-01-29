80e77f3ed0ce2863a20a4260239608e1f05fe701
Osama Mohammed <docosmo010@gmail.com>
2025-01-29 10:19:18 +0200

    fix: wrong syntax

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config = {
   darkMode: ["class"],
@@ -82,7 +83,7 @@ const config = {
       },
     },
   },
-  plugins: [await import("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
