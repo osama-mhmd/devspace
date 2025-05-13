f089ee4098812a536645faff04798493d3c87ad7
Osama Mohammed <docosmo010@gmail.com>
2025-05-13 14:11:02 +0300

    feat: better ux -> animation/smile icon/hide on success

--- src/app/app/workspace/[workspace_id]/survey.tsx
+++ src/app/app/workspace/[workspace_id]/survey.tsx
@@ -12,42 +12,79 @@ import {
 } from "@/components/ui/dialog";
 import checkSurveyFilledOrNot from "@/db/actions/surveys/check";
 import createSurvey from "@/db/actions/surveys/create";
+import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
-import { MessageCircleMore } from "lucide-react";
-import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { MessageCircleMore, Smile as SmileFace } from "lucide-react";
+import { useRef, useState } from "react";
 import { toast } from "sonner";
+
+const Smile = motion(SmileFace);
 
 export default function Survey() {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const { data, isPending } = useQuery({
+  const [state, setState] = useState<"success" | "can" | "loading" | "hidden">(
+    "can",
+  );
+  const { data: surveyFilled, isPending } = useQuery({
     queryKey: ["filled-state"],
     queryFn: async () => await checkSurveyFilledOrNot(),
   });
 
-  if (isPending) return null;
-  if (data) return null;
+  if (isPending || surveyFilled) return null;
 
   const submitServey = async () => {
     const data = textareaRef.current?.value ?? "";
 
     if (!data) return;
 
+    setState("loading");
+
     const result = await createSurvey(data);
 
     if (!result.ok) toast.error("Error occured while creating survey");
-    else
-      toast.success(
-        "Survey created successfully. We appreciate your feedback!",
-      );
+    else {
+      toast("Thanks, you appreciate filling out the survey. :)");
+      setState("success");
+      setTimeout(() => setState("hidden"), 2000);
+    }
+  };
+
+  const props = {
+    className:
+      "fixed bottom-6 end-6 flex items-center gap-2 cursor-pointer bg-blue-400 p-4 rounded-3xl",
+    size: 60,
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <MessageCircleMore
-          className="fixed bottom-6 end-6 flex items-center gap-2 cursor-pointer bg-blue-400 p-4 rounded-3xl"
-          size={60}
-        />
+        <div>
+          <AnimatePresence>
+            {(() => {
+              switch (state) {
+                case "success":
+                  return (
+                    <Smile
+                      className={cn("animate-bounce", props.className)}
+                      size={props.size}
+                      exit={{ opacity: 0 }}
+                    />
+                  );
+                case "can":
+                  return <MessageCircleMore {...props} />;
+                case "loading":
+                  return (
+                    <p {...props} style={{ width: 60, height: 60 }}>
+                      <span className="spinner w-full h-full"></span>
+                    </p>
+                  );
+                default:
+                  return null;
+              }
+            })()}
+          </AnimatePresence>
+        </div>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
